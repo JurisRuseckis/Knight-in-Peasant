089eccb330b795218f9b2b6a445ffefe25825f04
kanelitis <juris.ruseckis@gmail.com>
2014-11-21 17:14:28 +0200

    INIT

---
+++ Assets/Scripts/Enemy_controller.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+public var speed : float = 5.0f;
+public var player : Transform;
+player = GameObject.FindWithTag("Player").transform;
+
+
+function Start () {
+
+}
+
+function Update () {
+	var z : float = Mathf.Atan2( (player.transform.position.y - transform.position.y), (player.transform.position.x - transform.position.x) ) * Mathf.Rad2Deg - 90;
+	
+	transform.eulerAngles = new Vector3(0, 0, z);
+	
+	rigidbody2D.AddForce(gameObject.transform.up * speed);
+}
---
+++ Assets/Scripts/Player_controller.js
@@ -0,0 +1,34 @@
+﻿#pragma strict
+
+public var speed : float = 35.0f;
+private var anim : Animator;
+
+function Start () 
+{
+	anim = GetComponent("Animator");
+}
+
+function Update()
+{
+	if(Input.GetMouseButtonDown (0))
+	{
+		anim.SetTrigger("Attack");
+	}
+}
+
+function FixedUpdate () 
+{
+	//rotation - follow mouse
+	var mouse_position = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+	var rot : Quaternion = Quaternion.LookRotation(transform.position - mouse_position, Vector3.forward);
+	
+	transform.rotation = rot;
+	transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
+	rigidbody2D.angularVelocity = 0;
+	
+	//movement
+	var movement = Input.GetAxis("Vertical");
+	rigidbody2D.AddForce(gameObject.transform.up * speed * movement);
+	
+
+}
